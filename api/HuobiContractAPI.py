e3190c082e83a1064b275d4930759f36ea80322e
suishanwen <suishanwen@icloud.com>
2020-03-03 15:48:28 +0800

    test

--- api/HuobiContractAPI.py
+++ api/HuobiContractAPI.py
@@ -236,12 +236,15 @@ def send_order(acct_id, amount, symbol, _type, price=0):
     # amount = math.ceil(amount * price / 10)
     amount = math.ceil(amount)
     if security.get_volume() <= TRADE_LIMIT:
-        result = send_contract_order(contract_symbol, contract_type, contract_code, "", price, amount,
-                                     direction,
-                                     offset,
-                                     lever_rate,
-                                     order_price_type)
-
+        try:
+            result = send_contract_order(contract_symbol, contract_type, contract_code, "", price, amount,
+                                         direction,
+                                         offset,
+                                         lever_rate,
+                                         order_price_type)
+        except Exception as e:
+            print(str(e))
+            raise e
         order_symbol[str(result["data"]["order_id"])] = contract_symbol
         if "ok" == result["status"]:
             if direction == "buy":
